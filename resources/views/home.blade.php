c6ebb0f585909caa6ea456e78f6f6442e1ee3716
krunal121 <kmp9662@gmail.com>
2023-02-01 14:34:09 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test1</title>
+</head>
+<body>
+
+  <div class="container">
+    <div class="row">
+        <div class="mb-3">
+            <label for="" class="form-label">Name</label>
+            <input type="name" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com">
+          </div><br>
+        <div class="mb-3">
+          <label for="" class="form-label">Email</label>
+          <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com">
+          
+        </div>
+    </div>
+  </div>
+    
+</body>
+</html>
